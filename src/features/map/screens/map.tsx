ad920c687f011ede4a99eed1a29db324dbacc86b
Martin <myspammbox@seznam.cz>
2021-05-17 16:31:41 +0200

    Ep 182 done

--- src/features/map/screens/map.tsx
+++ src/features/map/screens/map.tsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import styled from "styled-components";
 import {Search} from "../components/search.component";
 import {LocationContext} from "../../../services/location/location.context";
@@ -15,14 +15,14 @@ const MapScreen = () => {
     const {restaurants = []} = useContext(RestaurantsContext);
     const [latDelta, setLatDelta] = useState(0);
 
-    const { lat, lng, viewport } = location;
+    const {lat, lng, viewport} = location;
 
     useEffect(() => {
-      const northeastLat = viewport.northeast.lat;
-      const southwestLat = viewport.southwest.lat;
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat;
 
-      const latDelta = northeastLat - southwestLat;
-      setLatDelta(latDelta);
+        const latDelta = northeastLat - southwestLat;
+        setLatDelta(latDelta);
     }, [location, viewport]);
 
 
@@ -32,13 +32,21 @@ const MapScreen = () => {
             <Map region={{
                 latitude: lat,
                 longitude: lng,
-                latitudeDelta: latDelta,
-                longitudeDelta: 0.1,
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.02,
             }}
-            />
-            {restaurants.map((restaurant) => {
-                return null;
-            })}
+            >
+                {restaurants.map((restaurant) => {
+                    return <Marker
+                        key={restaurant.name}
+                        title={restaurant.name}
+                        coordinate={{
+                            latitude: restaurant.geometry.location.lat,
+                            longitude: restaurant.geometry.location.lng,
+                        }}
+                    />
+                })}
+            </Map>
         </>
     )
 };
